a90e2d1d6c7f0428bd1126e397d621cddc241742
Rafael Malave <rafomalave@gmail.com>
2018-05-13 20:57:41 -0700

    edit form changes

--- lab-rafael/src/components/Category/CategoryEdit.jsx
+++ lab-rafael/src/components/Category/CategoryEdit.jsx
@@ -5,7 +5,7 @@ class CategoryEdit extends React.Component {
     super(props);
     this.state = {
       title: this.props.title,
-      title: this.props.number,
+      number: this.props.number,
       id: this.props.id
     }
 
@@ -30,7 +30,7 @@ class CategoryEdit extends React.Component {
   render() {
     return <form onSubmit={this.handleSubmit}>
       <input type="text" onChange={this.handleChange} value={this.state.title} id="title"></input>
-      <input type="number" onChange={this.handleChange} value={this.state.number} id="title"></input>
+      <input type="number" onChange={this.handleChange} value={this.state.number} id="number"></input>
       <button onClick={this.cancel}>Cancel</button>
       <button type="submit">Save</button>
     </form>
